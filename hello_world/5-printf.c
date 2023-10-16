e41e350ae91c2a7c34cfde48e7bb78c4bd685ca4
millienytorressanta <tmillieny@gmail.com>
2023-10-16 02:18:28 -0400

    l0

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 /**
  * main - this is the main function
- * Return: 0when successfull) 
+ * (Return): when successfull
  */
 
-int main(void) 
-{ 
-	printf("with proper grammar, but outcome is a piece of art");
-		return (0);
+int main(void)
+{	printf("with proper grammar,but outcome is a piece of art");
+		(return 0)
 }
