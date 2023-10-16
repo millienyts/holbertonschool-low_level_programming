6d96f44a1f4d63220f1be925a938cbe14d889eea
millienytorressanta <tmillieny@gmail.com>
2023-10-16 02:22:00 -0400

    l0

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 
 int main(void)
-{	printf("with proper grammar,but outcome is a piece of art");
-		(return 0)
+{	printf("with proper grammar,but outcome is a piece of art,|");
+		(return 0);
 }
