18b1b0d1fee2af897335861f66da32910dba6177
millienytorressanta <tmillieny@gmail.com>
2023-10-16 13:48:15 -0400

    kk

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
-	printf("Size of a int: %zu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(c));
-	printf("Size of a long long int: %zu bytes(s)\n", sizeof(d));
-	printf("Size of a float: %zu bytes(s)\n", sizeof(f));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
