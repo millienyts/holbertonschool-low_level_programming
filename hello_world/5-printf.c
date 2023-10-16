fe8818179fb42bfacb47e78d8173bdc4240fd935
millienytorressanta <tmillieny@gmail.com>
2023-10-16 10:23:06 -0400

    l0

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - A program that print a line using the printf function
- * (Return: when successfull)
+ * main - A program that print a line using the print function
+ * Return: 0 (Success)
  */
-
 int main(void)
-{	printf("with proper grammar,but outcome is a piece of art,|");
-		(return 0);
+{
+        printf("with proper grammar but the outcome is a piece of art,/n");
+        return (0);
 }
