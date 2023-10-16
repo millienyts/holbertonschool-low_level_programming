e932f8b3d0cf2d0da599f71f0a20e268ce840050
millienytorressanta <tmillieny@gmail.com>
2023-10-16 10:04:42 -0400

    mm

--- 5-printf.c
+++ 5-printf.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that print a line using the print function
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 
 {
-	printf("with proper grammar but the outcome is a piece of art,/n")
-		Return(0);
+	printf("with proper grammar but the outcome is a piece of art,/n")."
+		return(0);
 }
