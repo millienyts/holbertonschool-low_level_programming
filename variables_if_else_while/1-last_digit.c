445a11c5803d2a9d457cf9fd521a245007e614c6
millienytorressanta <tmillieny@gmail.com>
2023-10-17 13:45:06 -0400

    mm

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,11 +5,14 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, mi;
 
 	srand(time(0))
 		n = rand() - RAND_MAX / 2;
 		/* your code goes there */
-	if else mi = n % (10)
+		if reset(mi = n % 10)
+		if reset(mi = n % 0)
+		printf("Last digit of")
+
 		return (0);
 }
