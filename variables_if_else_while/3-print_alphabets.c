9c295ff0a9459b4ca9e488622d27e82b9efe62f0
millienytorressanta <tmillieny@gmail.com>
2023-10-17 20:19:57 -0400

    mm

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function of the task
+ * Return: always zero
+ */
+
+int main(void)
+{ char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar (i);
+
+for (i = 'A'; i <= 'z'; i++);
+putchar (i);
+putchar ('\n');
+return (0);
+
+}
