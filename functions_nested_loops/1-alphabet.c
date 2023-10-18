dd97a1810816995d0ebe3efb9b809843acd258cd
millienytorressanta <tmillieny@gmail.com>
2023-10-18 14:38:23 -0400

    hh

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+		while (i <= 9)
+		{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
