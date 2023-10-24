e3acbac47065811a749e8c89b5e67cbd253e4661
millienytorressanta <tmillieny@gmail.com>
2023-10-24 09:49:49 -0400

    ll

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,11 +9,10 @@
 void _puts(char *str)
 
 {
-	for (;*str != '\0'; str++)
+	for (; *str != '\0'; str++)
 	{
-	_putchar(*str);
+	putchar(*str);
 	}
-	_putchar('\n');
+	putchar('\n');
 
-	return(0);
 }
