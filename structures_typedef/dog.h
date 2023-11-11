65e3505e641f3008385267b4a1674258f04bd480
millienytorressanta <tmillieny@gmail.com>
2023-11-10 22:11:34 -0400

    kk

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - about dog
+ *@name: pointer to string, dog's name
+ *@age: float for age of dog
+ *@owner: pointer to name of owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
