0dec3277398018ddb75d175bcfd737a66e516c44
millienytorressanta <tmillieny@gmail.com>
2023-11-12 19:20:27 -0400

    kk

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+
+/**
+* print_name - prints a name
+* @name: input string
+* @f: function pointer casted to char pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || *f == NULL)
+		return;
+	(*f)(name);
+}
