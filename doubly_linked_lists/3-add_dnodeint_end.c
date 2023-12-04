db2f9f9b2fcde98b72391235d1aecd01b2813900
millienytorressanta <tmillieny@gmail.com>
2023-12-03 20:23:59 -0400

    lists

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,30 +1,40 @@
+#include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
 /**
-  * *add_dnodeint_end - add a node to the end of the list
-  * @head: head of the list
-  * @n: value to add to the l
-  * Return: pointer to the element added
-  */
+ * add_dnodeint_end- Add node to the end of Dlists
+ *
+ * @n: int from struct
+ * @head: pointer to nodes or null
+ * Return: Adress of new node
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp, *new;
+	dlistint_t *new_node, *temp_head;
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (!*head)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 	{
-		new->prev = NULL;
-		*head = new;
-		return (*head);
+		printf(" Memory failed to allocate\n");
+		return (NULL);
 	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-	tmp->prev = *head;
-	return (tmp);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		temp_head = *head;
+		while (temp_head->next != NULL)
+			temp_head = temp_head->next;
+		new_node->prev = temp_head;
+		temp_head->next = new_node;
+	}
+
+	return (new_node);
 }
